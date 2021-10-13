cc18732150fffb20bee087718ae4beb67299e134
Ryan Whitmore <ryanwhitmore.dev@gmail.com>
2021-10-13 12:54:23 -0600

    ready for deployment test

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './App.css';
+import logo from './img/ryanwhitmore.png';
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>Ryan Whitmore</h1>
+      <h2>Web Developer</h2>
+      <h3>
+        <a href="https://www.linkedin.com/in/whitmorespaceindustries/">
+          LinkedIn
+        </a>
+      </h3>
+      <h3>
+        <a href="https://github.com/warptrail">Github</a>
+      </h3>
+      <div className="frame">
+        <img src={logo} alt="logo" />
+      </div>
+    </div>
+  );
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import 'typeface-roboto';
-import './index.css';
-import App from './App/App';
 
-ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  document.getElementById('root')
-);
+import App from './App';
+
+ReactDOM.render(<App />, document.getElementById('root'));
